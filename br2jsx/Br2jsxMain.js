0290e7ec231165e0c9e360d8ef8347ac9a8bfa5d
Tanik.sam <tanik.sam@yandex.ru>
2021-11-01 12:30:26 +0300

    tatiana

---
+++ br2jsx/Br2jsxMain.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Br2jsx from './components/Br2jsx';
+
+let textBr=require('./textBr.json');
+
+ReactDOM.render(
+  <Br2jsx 
+    textBr={textBr}
+  />
+  , document.getElementById('container') 
+);
+
---
+++ br2jsx/components/Br2jsx.js
@@ -0,0 +1,39 @@
+﻿import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+
+import './Br2jsx.css';
+
+ 
+class Br2jsx extends React.Component {
+
+  static propTypes = {
+    
+    textBr: PropTypes.string.isRequired,
+    };
+
+    
+
+  textBreak = () => {
+  let br= this.props.textBr
+  let newArr=br.split('<br>')
+  console.log(newArr)
+  function ff(v,i,a){return v=="<br>" }
+  newArr.filter(ff); 
+  console.log(newArr)
+  return newArr.map(v => <p key={i}>{v}<br></br>
+  </p>)
+  }
+  
+  render() {
+  
+      return (
+        <div  className='Br2jsx'>
+{textBreak}
+      </div>
+    );
+  }
+
+}
+
+export default IShop3;
+
