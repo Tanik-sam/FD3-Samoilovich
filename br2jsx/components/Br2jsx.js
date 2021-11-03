69a80682c6d06b7bbaab8affa9658f712751c6cf
Tanik.sam <tanik.sam@yandex.ru>
2021-11-03 10:35:25 +0300

    tatiana

--- br2jsx/components/Br2jsx.js
+++ br2jsx/components/Br2jsx.js
@@ -14,16 +14,16 @@ class Br2jsx extends React.Component {
   render() {
       let br= this.props.textBr
  
-      let newStr=br.split(/<br>|<\/\br>/g).join('/<br>/')
-      let newArr=newStr.split('/')
+      let newArr=br.split(/<br>|<\/\br>/g)
+      
       console.log(newArr)
-       
-      function ff(v,i,a) {if (v=='<br>'){v=<br key={i}></br>} return (v)}
-      let r=newArr.map(ff)
-      console.log(r)
+
+ function ff(v,i,a) {return(<br key={i}>{v}</br>)}
+ let r=newArr.map(ff)
+console.log(r)
       return (
         <div  className='Br2jsx'>
-        {r}
+        {newArr}
        </div>
     );
   }
