51747fabd1650c07c74f36ce9d622d02c120e435
Tanik.sam <tanik.sam@yandex.ru>
2021-11-07 11:59:49 +0300

    tatiana

--- rainbowFrameHOC2/components/withRainbowFrame.js
+++ rainbowFrameHOC2/components/withRainbowFrame.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 function withRainbowFrame(colors){
 return  function withRainbowFrame(CompFrame) {
-  let wrappedText=this.props.children
-  colors.forEach(v=>{wrappedText=<div key={v} background={v} style={{border:"solid 20px ",padding:"10px",color:v,textAlign:"center"}}>{wrappedText}</div>})
+  
+  
       
     return props => {
-
+      let wrappedText={...props}
+      colors.forEach(v=>{wrappedText=<div key={v} background={v} style={{border:"solid 20px ",padding:"10px",color:v,textAlign:"center"}}>{wrappedText}</div>})(
+     
       <div style={{backgroundColor:colors[1]}}>
              
-      <CompFrame {...props} />
-      
+      <CompFrame  />
+     {wrappedText}
       </div>
-    };
+    );}
   };}
 
 
