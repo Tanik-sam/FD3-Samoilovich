f6eecd63546c1dc1eeb2e4849e3df9b341a951c7
Tanik.sam <tanik.sam@yandex.ru>
2021-11-09 00:50:53 +0300

    tatiana

--- rainbowFrameHOC2/components/withRainbowFrame.js
+++ rainbowFrameHOC2/components/withRainbowFrame.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 function withRainbowFrame(colors){
   return  function (CompFrame){
-     return props=>{
-      var k=<CompFrame {...props}/>    
-      
-     class Comp2{
-       
-       newV=()=>colors.forEach(v=>{k=<div key={v} background={v} style={{border:"solid 20px ",padding:"10px",color:v,textAlign:"center"}}>{k}</div>})
-       
-      } 
-      return Comp2 
+     return props=>(
+     
+     class Comp2  extends React.Component {
+       render() {
+         let k=this.props.children
+         console.log (k)
+         colors.forEach(v=>{k=<div key={v} background={v} style={{border:"solid 20px ",padding:"10px",color:v,textAlign:"center"}}>{k}</div>})
+             
+         return (<div>{k}</div>)}
      }
-  
-    }
-    
+      )
   }
+    
+}
 
 
  
