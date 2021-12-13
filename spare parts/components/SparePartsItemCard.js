adc54d328cc3e4d3dd6b4496449e934b4a7e6e8d
Tanik.sam <tanik.sam@yandex.ru>
2021-12-13 17:52:15 +0300

    tatiana

--- spare parts/components/SparePartsItemCard.js
+++ spare parts/components/SparePartsItemCard.js
@@ -277,7 +277,7 @@ setTimeout(this.defaultPosition,1000)
     else     {
     return (
       <div className="SparePartsItemCard">
-        <div className="SparePartsItemCardClose" ><img  onClick={this.defaultPosition} src="../close.png" width={30} height={30} />  </div>
+        <div className="SparePartsItemCardClose" ><img  onClick={this.clicked} src="../close.png" width={30} height={30} />  </div>
          <img className='Img' src={this.props.info.urlSP} width={430} height={290}/> 
         <legend className="SparePartsName">{"Изменение карточки клиента"}</legend>
           <table className='itemData2'>
